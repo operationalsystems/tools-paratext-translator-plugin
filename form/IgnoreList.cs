20cdc56778d659ed8fee82e968d87d1bdde3d044
Rico Alire <rico.alire@gmail.com>
2019-10-17 14:46:04 -0600

    added mock data for ignore list

--- form/IgnoreList.cs
+++ form/IgnoreList.cs
@@ -19,7 +19,12 @@ namespace translation_validation_framework.form
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            this.dataGridView1.Rows.Add("MAT, 9, 15", "(text), I", "Add to ignore list");
+            this.dataGridView1.Rows.Add("FOO, 9, 15", "foo, Bar", "This is an error");
+            this.dataGridView1.Rows.Add("GEN 10, 2", "(text), God", "Add to ignore list");
+            this.dataGridView1.Rows.Add("GEN, 2, 8", "(text); Adam", "Add to ignore list");
+            this.dataGridView1.Rows.Add("MAT, 10, 3", "(text); I", "Add to ignore list");
+            this.dataGridView1.Rows.Add("MAT, 15, 10", "(text); In", "This is an error");
         }
 
         private void button1_Click(object sender, EventArgs e)
