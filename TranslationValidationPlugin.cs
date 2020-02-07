ad1940888f14710a74eff8f0c9344054825c2715
Joel Schuster <joel.schuster@opsysinc.com>
2020-02-07 16:58:28 -0700

    Added enableWhen setting per Tim from Paratext input. The plugin now shows up under Tools in PT8 and under project menu Tools in PT9. No need for separate build.

--- TranslationValidationPlugin.cs
+++ TranslationValidationPlugin.cs
@@ -20,6 +20,7 @@ namespace TvpMain
     [AddIn("Translation Validation Plugin", Description = "Provides validation checks for translated text.", Version = "1.0", Publisher = "Biblica")]
     [QualificationData(PluginMetaDataKeys.menuText, "Translation Validation")]
     [QualificationData(PluginMetaDataKeys.insertAfterMenuName, "Tools|")]
+    [QualificationData(PluginMetaDataKeys.enableWhen, WhenToEnable.anyProjectActive)]
     [QualificationData(PluginMetaDataKeys.multipleInstances, CreateInstanceRule.always)]
     public class TranslationValidationPlugin : IParatextAddIn2
     {
