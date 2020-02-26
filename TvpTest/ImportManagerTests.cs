a64f695c0f0dd3f00eba03da1c5a1497b0b53d1c
Michael J. Kitchin <michael.kitchin@opsysinc.com>
2020-02-26 12:24:12 -0700

    DT428 - Test improvement.

--- TvpTest/ImportManagerTests.cs
+++ TvpTest/ImportManagerTests.cs
@@ -64,47 +64,7 @@ namespace TvpTest
         {
             base.AbstractTestSetup(TestContext);
 
-            // simple USFM verse parser
-            var nextChapterNum = 1; // all books start at chapter 1
-            var nextVerseNum = 0; // verse 0 is intro & other start matter
-
-            var verseBuilder = new StringBuilder();
-            var testVerseDictionary = new Dictionary<VerseLocation, string>();
-
-            foreach (var lineText in File.ReadAllLines(@"Resources\test-usfm-1.txt"))
-            {
-                var workText = lineText.Trim();
-
-                if (workText.StartsWith(@"\c ")
-                    || workText.StartsWith(@"\v "))
-                {
-                    var workParts = workText.Split(VerseDelimiter, 3);
-                    var workNum = int.Parse(workParts[1].Trim());
-
-                    if (workText.StartsWith(@"\c "))
-                    {
-                        nextChapterNum = workNum;
-                        nextVerseNum = 0;
-                    }
-                    else if (workText.StartsWith(@"\v "))
-                    {
-                        var verseLocation = new VerseLocation(TEST_BOOK_NUM, nextChapterNum, nextVerseNum);
-                        Assert.IsFalse(testVerseDictionary.ContainsKey(verseLocation),
-                            $"Duplicate verse location in test input: {verseLocation.VerseCoordinateText}");
-
-                        testVerseDictionary[verseLocation] = verseBuilder.ToString();
-                        verseBuilder.Clear();
-
-                        nextVerseNum = workNum;
-                    }
-                }
-
-                verseBuilder.AppendLine(lineText);
-            }
-
-            testVerseDictionary[new VerseLocation(TEST_BOOK_NUM, nextChapterNum, nextVerseNum)]
-                = verseBuilder.ToString();
-            VerseDictionary = testVerseDictionary;
+            VerseDictionary = GetUsfmVerses(@"Resources\test-usfm-1.txt");
             ReadVerses = new HashSet<VerseLocation>();
 
             // all expected verses have been read
@@ -182,6 +142,55 @@ namespace TvpTest
 
                 ctr++;
             }
+        }
+
+        /// <summary>
+        /// Simple USFM parser that extracts verses from a complete USFM file.
+        /// </summary>
+        /// <param name="filePath">USFM file path (required).</param>
+        /// <returns>Dictionary of verse locations to verse text.</returns>
+        private static IDictionary<VerseLocation, string> GetUsfmVerses(string filePath)
+        {
+            var nextChapterNum = 1; // all books start at chapter 1
+            var nextVerseNum = 0; // verse 0 is intro & other start matter
+
+            var verseBuilder = new StringBuilder();
+            var result = new Dictionary<VerseLocation, string>();
+
+            foreach (var lineText in File.ReadAllLines(filePath))
+            {
+                var workText = lineText.Trim();
+
+                if (workText.StartsWith(@"\c ")
+                    || workText.StartsWith(@"\v "))
+                {
+                    var workParts = workText.Split(VerseDelimiter, 3);
+                    var workNum = int.Parse(workParts[1].Trim());
+
+                    if (workText.StartsWith(@"\c "))
+                    {
+                        nextChapterNum = workNum;
+                        nextVerseNum = 0;
+                    }
+                    else if (workText.StartsWith(@"\v "))
+                    {
+                        var verseLocation = new VerseLocation(TEST_BOOK_NUM, nextChapterNum, nextVerseNum);
+                        Assert.IsFalse(result.ContainsKey(verseLocation),
+                            $"Duplicate verse location in test input: {verseLocation.VerseCoordinateText}");
+
+                        result[verseLocation] = verseBuilder.ToString();
+                        verseBuilder.Clear();
+
+                        nextVerseNum = workNum;
+                    }
+                }
+
+                verseBuilder.AppendLine(lineText);
+            }
+
+            result[new VerseLocation(TEST_BOOK_NUM, nextChapterNum, nextVerseNum)]
+                = verseBuilder.ToString();
+            return result;
         }
     }
 }
