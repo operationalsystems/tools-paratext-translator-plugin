d523d89ada9be0862b2998f1e967d8c63c5e2c33
Christian M. Macy <christian.macy@opsysinc.com>
2020-11-02 16:43:39 -0700

    DT-4: Add Interface for Repositories
    We need to support remote and local repositories. This
    interface describes what we expect from those repositories.

---
+++ CheckManager/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TvpMain.Check;
+
+namespace TvpMain.CheckManager
+{
+    interface IRepository
+    {
+        /// <summary>
+        /// This method gets check and fix items from a repository.
+        /// </summary>
+        /// <returns>A list of check and fix items that are available in the repository.</returns>
+        public List<CheckAndFixItem> GetCheckAndFixItems();
+
+        /// <summary>
+        /// This method adds a check and fix item to a repository.
+        /// </summary>
+        /// <param name="item">The check and fix item to add to the repository.</param>
+        public void AddCheckAndFixItem(CheckAndFixItem item);
+
+        /// <summary>
+        /// This method adds a check and fix item to a repository.
+        /// </summary>
+        /// <param name="item">The check and fix item to add to the repository.</param>
+        /// <returns>A task representing the result of the operation.</returns>
+        public Task AddCheckAndFixItemAsync(CheckAndFixItem item);
+    }
+}
