0be0f3c2cb6b84e2439d379da400b564ae557e37
Christian M. Macy <christian.macy@opsysinc.com>
2020-11-05 12:28:17 -0700

    DT-4: Add remote repository service and S3 service
    We may want to replace/modify the S3 service as the remote
    repository provider, so this is behind an interface, which also
    supports testing. For now tests are written directly against S3.
    
    AWS credentials are not included in this commit.

---
+++ CheckManager/S3Service.cs
@@ -0,0 +1,72 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Model.Internal.MarshallTransformations;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TvpMain.CheckManager
+{
+    public class S3Service : IRemoteService
+    {
+        // Read-only PPM repository and CLI AWS configuration parameters.
+        const String accessKey = "";
+        const String secretKey = "";
+        const String bucketName = "biblica-tvp-checks-repo";
+
+        /// <summary>
+        /// The temporary AWS credentials we use for S3 requests.
+        /// </summary>
+        public AmazonS3Client S3Client { get; set; } = new AmazonS3Client(accessKey, secretKey, RegionEndpoint.USEast1);
+
+        public List<string> ListAllFiles()
+        {
+            return ListAllFilesAsync().Result;
+        }
+
+        public async Task<List<string>> ListAllFilesAsync()
+        {
+            List<string> checkFileNames = new List<string>();
+            ListObjectsV2Request request = new ListObjectsV2Request
+            {
+                BucketName = bucketName,
+                MaxKeys = 10
+            };
+            ListObjectsV2Response response;
+            do
+            {
+                response = await S3Client.ListObjectsV2Async(request);
+
+                // Process the response.
+                foreach (S3Object entry in response.S3Objects)
+                {
+                    checkFileNames.Add(entry.Key);
+                }
+                request.ContinuationToken = response.NextContinuationToken;
+            } while (response.IsTruncated);
+
+            return checkFileNames;
+        }
+
+        public Stream GetFileStream(string key)
+        {
+            return GetFileStreamAsync(key).Result;
+        }
+
+        public async Task<Stream> GetFileStreamAsync(string key)
+        {
+            GetObjectRequest getObjectRequest = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key
+            };
+
+            GetObjectResponse getObjectResponse = await S3Client.GetObjectAsync(getObjectRequest);
+
+            return getObjectResponse.ResponseStream;
+        }
+    }
+}
---
+++ TvpTest/CheckManager/S3ServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TvpMain.CheckManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvpTest
+{
+    [TestClass()]
+    public class S3ServiceTests
+    {
+        S3Service S3Service { get; set; } = new S3Service();
+
+        [TestMethod()]
+        public void ListFilesTest()
+        {
+            var files = S3Service.ListAllFiles();
+            Assert.IsTrue(files.Contains("test.txt"));
+        }
+
+        [TestMethod()]
+        public void GetFileTest()
+        {
+            var file = S3Service.GetFileStreamAsync("test.txt");
+
+            Assert.IsTrue(file.Result.Length > 0);
+        }
+    }
+}
