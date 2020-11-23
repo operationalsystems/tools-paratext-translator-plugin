0fc035ef2a0ba702f268a59ccbfeb902572f623a
Joel Schuster <joel.schuster@opsysinc.com>
2020-11-23 14:52:37 -0700

    DT-6 added script engine and initial impl of check runner

--- Check/CheckAndFixRunner.cs
+++ Check/CheckAndFixRunner.cs
@@ -3,21 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TvpMain.Reference;
 
 namespace TvpMain.Check
 {
     public class CheckAndFixRunner : ICheckAndFixRunner
     {
-        
-
         public List<CheckResultItem> ExecCheckAndFix(string text, CheckAndFixItem checkAndFixItem)
         {
+            Regex checkRegex = new Regex( checkAndFixItem.CheckRegex);
+            MatchCollection matches = checkRegex.Matches(text);
+            List<CheckResultItem> checkResultItems = new List<CheckResultItem>();
+
+            foreach ( Match match in matches)
+            {
+                CheckResultItem checkResultItem = new CheckResultItem(checkAndFixItem.Description, 
+                    match.Value,
+                    match.Index, 
+                    CheckType.MissingSentencePunctuation,
+                    (int) ScriptureReferenceErrorType.LooseFormatting
+                    );
+
+                if (!String.IsNullOrEmpty(checkAndFixItem.FixRegex))
+                {
+                    checkResultItem.FixText = match.Result(checkAndFixItem.FixRegex);
+                }
+                checkResultItems.Add(checkResultItem);
+            }
+
             using (var engine = new V8ScriptEngine())
             {
-
+                engine.AddHostObject("checkResultItems", checkResultItems);
+                engine.Execute(checkAndFixItem.CheckScript);
+                checkResultItems = engine.Script.checkResultItems;
             }
-            return new List<CheckResultItem>();
+
+            return checkResultItems;
+            //return new List<CheckResultItem>(); 
         }
     }
 }
