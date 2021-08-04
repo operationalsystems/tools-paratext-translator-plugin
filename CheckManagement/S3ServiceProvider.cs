97be0c0ee74072412d3c6a22d935ce93e2a9ae2d
Christian M. Macy <christian.macy@opsysinc.com>
2021-08-04 10:52:29 -0700

    DT-273: Add docs to S3ServiceProvider.cs about singleton usage

--- CheckManagement/S3ServiceProvider.cs
+++ CheckManagement/S3ServiceProvider.cs
@@ -7,13 +7,23 @@ namespace TvpMain.CheckManagement
     /// </summary>
     public sealed class S3ServiceProvider : S3Service
     {
+        /// <summary>
+        /// A lazy-loaded instance. This instance is created the first time it's accessed, and retained. This enabled
+        /// multiple usages of the same instance rather than creating an instance for each usage.
+        /// </summary>
         private static readonly Lazy<S3ServiceProvider>
             InstanceLazy =
                 new Lazy<S3ServiceProvider>
                     (() => new S3ServiceProvider());
 
+        /// <summary>
+        /// Returns the current instance rather than creating a new one.
+        /// </summary>
         public static S3Service Instance => InstanceLazy.Value;
 
+        /// <summary>
+        /// Private CTOR for instance initialization
+        /// </summary>
         private S3ServiceProvider()
         {
         }
